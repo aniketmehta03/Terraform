5b830b8a404e5cdb4d490fa019028b24ec84aa33
aniketmehta03 <aniketmehta036@gmail.com>
2025-04-28 10:39:20 +0000

    first-terraform-resource

---
+++ terraform-first-resource/terraform.tf
@@ -0,0 +1,11 @@
+provider "github" {
+
+}
+
+resource "github_repository" "my-first-terraform" {
+  name         = "my-first-repo-terraform"
+  description  = "This is my first repository from terraform"
+
+  visibility   = "public"
+  auto_init = true
+}
